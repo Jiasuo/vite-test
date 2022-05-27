fc4d4a8dcabead1ff3cd79f8f44076ba13cc95d9
jiasuo <jiasuo91@gmail.com>
2022-05-27 22:22:41 +0200

    new vite project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {onMounted} from "vue"
+import gsap from "gsap"
+onMounted(() => {
+  gsap.from("#target", {x:-100})
+})
+</script>
+
+<template>
+<div id="target">Hello world</div>
+</template>
+
+<style>
+</style>
